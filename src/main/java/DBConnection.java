5acd0d4f13f927e356ee10eecede8ace955ac1a8
Zayan <zayan_ahmad@hotmail.com>
2025-08-07 15:17:07 -0500

    create class and connect to database

---
+++ src/main/java/DBConnection.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnection {
+    public static Connection getConnection() throws IOException, SQLException {
+
+        Properties props = DBConfig.loadProperties();
+        String url = props.getProperty("db.url");
+        String username = props.getProperty("db.username");
+        String password = props.getProperty("db.password");
+
+        return DriverManager.getConnection(url, username, password);
+    }
+}
