3204cc63b402f2061c57237f5aba974481b82511
Zayan <zayan_ahmad@hotmail.com>
2025-08-07 15:16:42 -0500

    create class and write loadProperties method

---
+++ src/main/java/DBConfig.java
@@ -0,0 +1,16 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class DBConfig {
+    private static final String CONFIG_FILE = "config/db.properties";
+
+    public static Properties loadProperties() throws IOException {
+        Properties props = new Properties();
+        try (FileInputStream fis = new FileInputStream(CONFIG_FILE)) {
+            props.load(fis);
+        }
+        return props;
+    }
+
+}
