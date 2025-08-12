72c01e927ac0130944634055decb597ffc933034
Zayan <zayan_ahmad@hotmail.com>
2025-08-12 12:44:34 -0500

    change package name from database to dao

--- src/main/java/dao/DBConfig.java
+++ src/main/java/dao/DBConfig.java
@@ -1,4 +1,4 @@
-package database;
+package dao;
 
 import java.io.FileInputStream;
 import java.io.IOException;
--- src/main/java/dao/DBConnection.java
+++ src/main/java/dao/DBConnection.java
@@ -1,4 +1,4 @@
-package database;
+package dao;
 
 import java.io.IOException;
 import java.sql.Connection;
